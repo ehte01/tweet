973c3a24ae33522ac02b9df2d64ccddf8476dc4a
ehte <ehteahmad@gmail.com>
2023-07-28 02:29:23 +0530

    version6.2

--- server/config/database/connection.js
+++ server/config/database/connection.js
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose'
+c
 
 export const connectToDatabase = () => {
     mongoose.connect('mongodb+srv://ehteahmad:0gvXTN5yq8HrSluS@cluster0.h5s6ofc.mongodb.net/twit').then(()=>{
-    console.log('connected to mongodb database');
+    console.log('connected to mongod database');
     }).catch(()=>{
         console.log('problem connected to mongodb database');
     })
